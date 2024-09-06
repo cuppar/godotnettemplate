b1264c3ab7bf70b7e4eaf1eacf0731cc76a39e72
cuppar <cuppar@163.com>
2024-09-06 23:24:18 +0800

    add camera prefab

--- globals/Game.cs
+++ globals/Game.cs
@@ -1,6 +1,13 @@
-﻿namespace GodotNetTemplate.Globals;
+﻿using System;
+
+namespace GodotNetTemplate.Globals;
 
 public static class Game
 {
+    public static event Action<float>? ShakeCameraEvent;
 
+    public static void ShakeCamera(float amount)
+    {
+        ShakeCameraEvent?.Invoke(amount);
+    }
 }
