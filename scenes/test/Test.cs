ca5bc2e146208eae86f53d51845d7fd9ec011814
cuppar <cuppar@163.com>
2024-09-12 22:57:12 +0800

    add imgui addon

--- scenes/test/Test.cs
+++ scenes/test/Test.cs
@@ -1,7 +1,27 @@
 using Godot;
+#if IMGUI
+using ImGuiGodot;
+using ImGuiNET;
+#endif
 
 namespace GodotNetTemplate;
 
 public partial class Test : Node2D
 {
+    public override void _Ready()
+    {
+        base._Ready();
+#if IMGUI
+        ImGuiGD.Connect(OnImGuiLayout);
+#endif
+    }
+
+#if IMGUI
+    private void OnImGuiLayout()
+    {
+        ImGui.Begin("ImGui on Godot 4");
+        ImGui.Text("hello world");
+        ImGui.End();
+    }
+#endif
 }
