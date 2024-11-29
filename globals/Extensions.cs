211d36947be1e795ee8706271ca493c9b1702b00
cuppar <cuppar@163.com>
2024-11-29 18:26:13 +0800

    add Extensions usage examples

--- globals/Extensions.cs
+++ globals/Extensions.cs
@@ -7,12 +7,44 @@ namespace GodotNetTemplate.Globals.Extensions;
 
 public static class Extensions
 {
+    /* EnsureReadyAsync Usage Example:
+    public partial class Test : Node2D
+    {
+        // live template shortcut: "es"
+        #region Health
+
+        private double _health;
+
+        [Export]
+        public double Health
+        {
+            get => _health;
+            set => SetHealth(value);
+        }
+
+        private async void SetHealth(double value)
+        {
+            await this.EnsureReadyAsync();
+            _health = value;
+        }
+
+        #endregion
+    }*/
     public static async Task EnsureReadyAsync(this Node node)
     {
         if (!node.IsNodeReady())
             await node.ToSignal(node, Node.SignalName.Ready);
     }
 
+    /* DelayAsync Usage Example:
+    public partial class Test : Node2D
+    {
+        async Task Method()
+        {
+            await this.DelayAsync(10);
+            // Do Something
+        }
+    }*/
     public static async Task DelayAsync(this Node node, float seconds)
     {
         var tree = node.GetTree();
@@ -20,6 +52,47 @@ public static class Extensions
         await node.ToSignal(timer, SceneTreeTimer.SignalName.Timeout);
     }
 
+    /* EnsureToolReadyAsync Usage Example:
+    [Tool]
+    public partial class Test : Node2D, ISerializationListener
+    {
+        private readonly TaskCompletionSource _toolReadyTCS = new();
+
+        #region ISerializationListener Members
+
+        public void OnBeforeSerialize()
+        {
+            SetMeta(MetaNames.Reloading, true);
+        }
+
+        public void OnAfterDeserialize()
+        {
+            SetMeta(MetaNames.Reloading, false);
+            _toolReadyTCS.SetResult();
+        }
+
+        #endregion
+
+        // live template shortcut: "tes"
+        #region Health
+
+        private double _health;
+
+        [Export]
+        public double Health
+        {
+            get => _health;
+            set => SetHealth(value);
+        }
+
+        private async void SetHealth(double value)
+        {
+            await this.EnsureToolReadyAsync(_toolReadyTCS);
+            _health = value;
+        }
+
+        #endregion
+    }*/
     public static async Task EnsureToolReadyAsync(this Node node, TaskCompletionSource toolReadyTCS)
     {
         if (!node.HasMeta(MetaNames.Reloading))
@@ -36,6 +109,17 @@ public static class Extensions
 
 public static class SignalExtensions
 {
+    /* SignalExtensions.WhenAll Usage Example:
+    public partial class Test : Node
+    {
+        async void Method()
+        {
+            var task1 = ToSignal(this, SignalName.Ready);
+            var task2 = ToSignal(this, SignalName.TreeEntered);
+            await SignalExtensions.WhenAll(task1, task2);
+            // do something
+        }
+    }*/
     public static async Task WhenAll(params SignalAwaiter[] awaiterArray)
     {
         var tasks = from awaiter in awaiterArray
