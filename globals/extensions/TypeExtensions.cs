02d867f4a5df4801cc51bafc4ace063aff275627
cuppar <cuppar@163.com>
2025-03-01 19:47:33 +0800

    add TypeExtensions.cs

---
+++ globals/extensions/TypeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MonsterTower.Globals.Extensions;
+
+public static class TypeExtensions
+{
+    /// <summary>
+    /// 使用Activator创建实例
+    /// </summary>
+    public static object CreateInstance(this Type type, bool nonPublic = true)
+    {
+        return Activator.CreateInstance(type, nonPublic)!;
+    }
+
+    /// <summary>
+    /// 使用Activator创建实例，并转换为T类型
+    /// </summary>
+    public static T CreateInstance<T>(this Type type, bool nonPublic = true)
+    {
+        return (T)type.CreateInstance(nonPublic);
+    }
+}
